15d34540fa7036158c4f81cea64f720edddb3205
jmgfx <41513139+jmgfx@users.noreply.github.com>
2022-02-15 16:54:47 +0800

    Added value var

--- k6metrics-to-junit.js
+++ k6metrics-to-junit.js
@@ -1,11 +1,12 @@
 function generateXMLFromMetrics(data, options) {
   var junitMetric = [];
-  var name = options && options.name ? options.name : 'k6 metrics'
+  var name = options && options.name ? options.name : 'k6 metrics';
+  var value = options && options.value ? options.value : 'p(90)';
 
   for (var metric in data.metrics) {
     if (data.metrics[metric]['values']['p(90)']) {
       junitMetric.push(
-        '<testcase classname="' + metric + '" name="' + metric + ' P(90)' + '" time="' + data.metrics[metric]['values']['p(90)']/1000 + '"/>'
+        '<testcase classname="' + metric + '" name="' + metric + ' ' + value + '" time="' + data.metrics[metric]['values'][value]/1000 + '"/>'
       )
     }
   }
