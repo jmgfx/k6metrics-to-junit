577f13cb854bebb4a9878df7e5f012d42871e6e0
jmgfx <41513139+jmgfx@users.noreply.github.com>
2022-02-16 17:42:51 +0800

    Added separate testsuite for http_req_duration

--- k6metrics-to-junit.js
+++ k6metrics-to-junit.js
@@ -1,20 +1,31 @@
 function generateXMLFromMetrics(data, options) {
   var junitMetric = [];
+  var httpReqDuration = [];
   var name = options && options.name ? options.name : 'k6 metrics';
   var value = options && options.value ? options.value : 'p(90)';
 
   for (var metric in data.metrics) {
     if (data.metrics[metric]['values']['p(90)']) {
-      junitMetric.push(
-        '<testcase classname="' + metric + '" name="' + metric + ' ' + value + '" time="' + data.metrics[metric]['values'][value]/1000 + '"/>'
-      )
+      if (metric.includes('http_req_duration')) {
+        httpReqDuration.push(
+          '<testsuite name="' + metric + '">'
+          + '<testcase classname="' + metric + '" name="' + metric + ' ' + value + '" time="' + data.metrics[metric]['values'][value]/1000 + '"/>'
+          + '</testsuite>'
+        )
+      } else {
+        junitMetric.push(
+          '<testcase classname="' + metric + '" name="' + metric + ' ' + value + '" time="' + data.metrics[metric]['values'][value]/1000 + '"/>'
+        )
+      }
     }
   }
 
-  return '<?xml version="1.0"?><testsuites><testsuite name="'
-      + name + '">\n'
-      + junitMetric.join('\n')
-      + '\n</testsuite></testsuites>';
+  return '<?xml version="1.0"?><testsuites>'
+        + httpReqDuration.join('\n')
+        + '<testsuite name="' + name + '">'
+        + junitMetric.join('\n')
+        + '</testsuite>'
+        + '\n</testsuites>';
 }
 
 exports.MetricsToJunit = generateXMLFromMetrics
