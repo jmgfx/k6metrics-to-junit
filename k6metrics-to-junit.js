010d6e82cdf60a8659c998e3eefe6ae8391968fd
jmgfx <41513139+jmgfx@users.noreply.github.com>
2022-02-16 18:19:15 +0800

    Added generateXMLFromReqDuration

--- k6metrics-to-junit.js
+++ k6metrics-to-junit.js
@@ -1,5 +1,23 @@
 function generateXMLFromMetrics(data, options) {
   var junitMetric = [];
+  var name = options && options.name ? options.name : 'k6 metrics';
+  var value = options && options.value ? options.value : 'p(90)';
+
+  for (var metric in data.metrics) {
+    if (data.metrics[metric]['values']['p(90)']) {
+      junitMetric.push(
+        '<testcase classname="' + metric + '" name="' + metric + ' ' + value + '" time="' + data.metrics[metric]['values'][value]/1000 + '"/>'
+      )
+    }
+  }
+
+  return '<?xml version="1.0"?>\n'
+    + '<testsuites><testsuite name="' + name + '">\n'
+    + junitMetric.join('\n')
+    + '</testsuite></testsuites>';
+}
+
+function generateXMLFromReqDuration(data, options) {
   var httpReqDuration = [];
   var name = options && options.name ? options.name : 'k6 metrics';
   var value = options && options.value ? options.value : 'p(90)';
@@ -8,12 +26,6 @@ function generateXMLFromMetrics(data, options) {
     if (data.metrics[metric]['values']['p(90)']) {
       if (metric.includes('http_req_duration')) {
         httpReqDuration.push(
-          '<testsuites><testsuite name="' + metric + '">'
-          + '<testcase classname="' + metric + '" name="' + metric + ' ' + value + '" time="' + data.metrics[metric]['values'][value]/1000 + '"/>'
-          + '</testsuite></testsuites>'
-        )
-      } else {
-        junitMetric.push(
           '<testcase classname="' + metric + '" name="' + metric + ' ' + value + '" time="' + data.metrics[metric]['values'][value]/1000 + '"/>'
         )
       }
@@ -21,10 +33,10 @@ function generateXMLFromMetrics(data, options) {
   }
 
   return '<?xml version="1.0"?>\n'
-        + httpReqDuration.join('\n')
-        + '\n<testsuites><testsuite name="' + name + '">\n'
-        + junitMetric.join('\n')
-        + '</testsuite></testsuites>';
+    + '<testsuites><testsuite name="' + name + '">\n'
+    + httpReqDuration.join('\n')
+    + '</testsuite></testsuites>';
 }
 
 exports.MetricsToJunit = generateXMLFromMetrics
+exports.ReqDurationToJunit = generateXMLFromReqDuration
